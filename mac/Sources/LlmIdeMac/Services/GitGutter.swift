2ecd56e98bdc8c99641b29aa0a416c3d757b8028
dnsmalla <malla.dinesh@seichogroup.com>
2026-09-03 19:04:52 +0900

    feat(mac): add MonacoBridge JS->Swift message types

--- mac/Sources/LlmIdeMac/Services/GitGutter.swift
+++ mac/Sources/LlmIdeMac/Services/GitGutter.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum GitGutter {
-    enum Mark { case added, modified }
+    enum Mark { case added, modified, deleted }
 
     /// New-side line number → change mark, derived from a unified diff.
     /// A run of inserts adjacent to deletes is "modified"; pure inserts are "added".
